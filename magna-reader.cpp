1e154663559b418c43cada1b3335e69740d1f7df
viklo <viggo@gstromberg.nu>
2021-01-29 09:03:07 +0100

    command line

--- magna-reader.cpp
+++ magna-reader.cpp
@@ -1,16 +1,31 @@
 #include <cstdlib>
+#include <vector>
 #include "utils.h"
 #include "gui.h"
+
+std::vector<std::wstring> getCmdLineArgs(const wchar_t* cmdLine)
+{
+	int argc;
+	const UHandle<wchar_t**, LocalFree> strs(CommandLineToArgvW(cmdLine, &argc));
+	if (!strs) throw std::runtime_error("Failed to convert command line to argv");
+	std::vector<std::wstring> vec;
+	for (int i = 0; i < argc; i++)
+	{
+		vec.emplace_back(strs.get()[i]);
+	}
+	return vec;
+}
 
 int WINAPI wWinMain(
 	HINSTANCE /*hInstance*/,
 	HINSTANCE /*hPrevInstance*/,
-	PWSTR /*pCmdLine*/,
+	PWSTR pCmdLine,
 	int /*nCmdShow*/
 )
 {
 	try
 	{
+		const auto args = getCmdLineArgs(pCmdLine);
 		MainWindow mw;
 		while (mw) mw.update();
 	}
